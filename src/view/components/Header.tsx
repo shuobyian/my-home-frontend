9a25fb9a4f4c376a1cfd66eb3a3cfe0b34f519a0
shuobyian <chickby@gmail.com>
2023-08-14 01:19:44 +0900

    header 메시지 추가

--- src/view/components/Header.tsx
+++ src/view/components/Header.tsx
@@ -4,9 +4,21 @@ export function Header() {
   return (
     <div style={{ display: "fixed", top: 0, padding: "10px" }}>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography variant='h3' fontWeight={700}>
-          놀러와 마이홈 제작 계산기
-        </Typography>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "auto",
+          }}
+        >
+          <Typography variant='h3' fontWeight={700}>
+            놀러와 마이홈 제작 계산기
+          </Typography>
+          <Typography>
+            물품 이름과 수량을 입력해주세요. 기초 재료를 확인하실 수 있습니다.
+          </Typography>
+          <Typography>빈칸 입력 시 모든 물품 확인이 가능합니다.</Typography>
+        </div>
         <div />
       </div>
     </div>
